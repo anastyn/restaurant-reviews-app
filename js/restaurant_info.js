4527a8675660a562ffdae3e0cbf66b5bafae65ff
Andriy Nastyn <andriy.nastyn@ctbto.org>
2018-03-09 21:24:54 +0100

    feat: Style restaurant review cards

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -118,21 +118,34 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
+  const liTitle = document.createElement('div');
+  liTitle.className = 'title';
+
   const name = document.createElement('p');
+  name.className = 'name';
   name.innerHTML = review.name;
-  li.appendChild(name);
+  liTitle.appendChild(name);
 
   const date = document.createElement('p');
   date.innerHTML = review.date;
-  li.appendChild(date);
+  date.className = 'date';
+  liTitle.appendChild(date);
+
+  li.appendChild(liTitle);
+
+  const liContent = document.createElement('div');
+  liContent.className = 'content';
 
   const rating = document.createElement('p');
+  rating.className = 'rating';
   rating.innerHTML = `Rating: ${review.rating}`;
-  li.appendChild(rating);
+  liContent.appendChild(rating);
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
-  li.appendChild(comments);
+  liContent.appendChild(comments);
+
+  li.appendChild(liContent);
 
   return li;
 }
